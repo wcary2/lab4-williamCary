06671c43bfe78e2fd075f14a8b68eeed03068782
wcary2 <43153826+wcary2@users.noreply.github.com>
2018-10-08 20:22:52 -0600

    Update VectorDriver.h

--- VectorDriver.h
+++ VectorDriver.h
@@ -1,13 +1,6 @@
 /* File: VectorDriver.cpp
- * Athr: Dimitri Zarzhitsky
- * Date: October 16, 2002
- *
- * Desc: provides the basic framework and examples for an introduction to
- *       the STL::vector, iterators, and the algorithms facilities.
- *
- * Edited by: Rafe Cooley
- * Date: October 1, 2017 (the future)
- *
+ * Athr: William Cary
+ * Date: October 8 2018
  */
 typedef unsigned long ulong;
 
@@ -56,20 +49,21 @@ short compute_sum(const vector<short> &data) {
 }
 
 void add_numbers(vector<short> &data) {
-	srand(time(NULL));
+	srand(time(NULL)); //initizalizes seed for random numbers based on time
 	for (unsigned i = 0; i < 10; i++) {
-		data.push_back(rand() % 100);
+		data.push_back(rand() % 100); //use push_back to insert random numbers from 0 to 99
 	}
 	return;
 }
 
 void print_even(vector<short> &data) {
-	if (data.empty()) {
+	if (data.empty()) { //checks if data is empty and if it is it prints "<empty>"
 		cout << "<empty>";
+		return;
 	}
 	else {
 		cout << "<" << data.at(0);
-		for (ulong i = 2; i<data.size(); i+=2) {
+		for (ulong i = 2; i<data.size(); i+=2) { // adds two to i so that it skips every other index in the vector
 			cout << ", " << data.at(i);
 		}
 		cout << ">" << endl;
@@ -77,9 +71,9 @@ void print_even(vector<short> &data) {
 }
 
 bool is_present(vector<short> &data, short val) {
-	if (!data.empty()) {
-		for (vector<short>::iterator it = data.begin(); it != data.end(); it++) {
-			if (*it == val) {
+	if (!data.empty()) { //quick test if empty: returns false if it is.
+		for (vector<short>::iterator it = data.begin(); it != data.end(); it++) { //uses iterator to increase efficiency over at function for vectors
+			if (*it == val) { //dereference iterator and checks the value to see if
 				cout << val << " Present in vector " << endl;
 				return true;
 			}
